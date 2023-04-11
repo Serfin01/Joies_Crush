4032702b4d0756cc87efa0004ad1fd050e58513d
Sergi Lozano <sergilozano96@gmail.com>
2023-04-11 15:35:48 +0200

    home clean and logincontroller

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,39 @@
+<div class="footer">
+    <div>
+        <h4>SOBRE JOIES CRUSH</h4>
+        <p>Coleccions</p>
+        <p>Catàleg d’arrecades</p>
+        <p>Galería d’estils</p>
+        <p>Sobre nosaltres</p>
+        <p>Contacte</p>
+    </div>
+    <div>
+        <h4>LEGAL</h4>
+        <p>Condicions generals</p>
+        <p>Pol·lítica de privacitat</p>
+        <p>Pol·lítica de cookies</p>
+    </div>
+    <div>
+        <h4>AJUDA</h4>
+        <p>Métodes de pagament</p>
+        <p>Termes de servei</p>
+        <p>Devolucions</p>
+    </div>
+    <div>
+        <div class="contactos">
+            <p>joiescrush@gmail.com</p>
+            <p>+34 000 000 000</p>
+            <p>C/ la vida es dura 12</p>
+        </div>
+        <div>
+            <i class="fa fa-instagram redes"></i>
+            <i class="fa fa-youtube-play redes"></i>
+            <i class="fa fa-whatsapp redes"></i>
+            <i class="fa fa-google redes"></i>
+            {{-- <img src="" alt="instagram">
+            <img src="" alt="youtube">
+            <img src="" alt="whatsapp">
+            <img src="" alt="gmail"> --}}
+        </div>
+    </div>
+</div>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@0;1&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@400;700&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+    <title>Joies Crush</title>
+</head>
+<body>
+    <div class="preHeader">
+        <p>ENVIAMENT GRATUÏT. Comandes superiors a 35€</p>
+    </div>
+    <div class="header">
+        <div>
+            <img src="{{ asset('img/icons/JoyesCrushLogo 1.png') }}" alt="JoiesCrush Logo">
+            <h1>Joies Crush</h1>
+        </div>
+        <div>
+            <div class="search-container">
+                <form id="form" role="search">
+                    <input type="search" id="query" name="q"
+                    placeholder="Buscar..."
+                    aria-label="Search through site content">
+                    <button>
+                        <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
+                      </button>
+                    </form>
+            </div>
+            <div>
+                <img class="iconos" src="{{ asset('img/icons/perfil.png') }}" alt="icono perfil">
+                <img class="iconos" src="{{ asset('img/icons/corazon.png') }}" alt="icono favoritos">
+                <img class="iconos" src="{{ asset('img/icons/shopping-bag 1.png') }}" alt="icono tienda">
+                {{-- <img class="iconos" src="{{ asset('img/icons/idioma 1.png') }}" alt="icono idioma"> --}}
+            </div>
+        </div>
+    </div>
