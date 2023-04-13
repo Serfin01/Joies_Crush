7d774c6bbf800bb84cb5e9e62a10cd650f5ddcdc
Sergi Lozano <sergilozano96@gmail.com>
2023-04-13 16:48:34 +0200

    error404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Joies Crush</title>
+</head>
+<body>
+    <div class="error">
+        <div>
+            <img src="{{ asset('img/dibujos/migcor1.png') }}" alt="medio corazon">
+            <h2>Oops!</h2>
+            <img src="{{ asset('img/dibujos/migcor2.png') }}" alt="medio corazon">
+        </div>
+        <h3>- ERROR 404 -</h3>
+        <p>Ho sento. Sembla que la pàgina que estàs<br> buscant no existeix...</p>
+        <a href="">Torna a l’inici</a>
+    </div>
+</body>
+</html>
